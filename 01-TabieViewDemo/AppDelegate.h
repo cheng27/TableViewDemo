3c3ecf6ecb4a57db996664efe55e3a359bda1a09
程玉娟 <1009449682@qq.com>
2015-12-02 15:28:58 +0800

    Initial Commit

---
+++ 01-TabieViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-TabieViewDemo
+//
+//  Created by qingyun on 15/12/2.
+//  Copyright (c) 2015年 阿六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-TabieViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01-TabieViewDemo
+//
+//  Created by qingyun on 15/12/2.
+//  Copyright (c) 2015年 阿六. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
